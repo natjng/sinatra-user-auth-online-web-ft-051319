623d8b1cf6afeb9c4b2dd30192892d3204e01bed
Natalie <natsetc@gmail.com>
2019-07-05 20:41:11 -0700

    add notes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,10 +18,14 @@ class ApplicationController < Sinatra::Base
 
   post '/registrations' do
     puts params
+    # will show params from form in terminal
 
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
+    # use data in params to create a new user
+
     session[:user_id] = @user.id
+    # log in new user by setting user id it to the session[:user_id]
     
     redirect '/users/home'
   end
@@ -36,9 +40,12 @@ class ApplicationController < Sinatra::Base
     puts params
 
     @user = User.find_by(email: params[:email], password: params[:password])
+    # find user with email and password data from params
+
     if @user
       session[:user_id] = @user.id
       redirect '/users/home'
+      # if user is found in db, log in user by setting user id to session[:user_id] and redirect use to get '/users/home' route
     end
     redirect '/sessions/login'
   end
@@ -46,11 +53,12 @@ class ApplicationController < Sinatra::Base
   get '/sessions/logout' do
     session.clear
     redirect '/'
+    # log out user by clearing data in session hash and redirect to home page
   end
 
   get '/users/home' do
-
     @user = User.find(session[:user_id])
     erb :'/users/home'
+    # find current user using :user_id in session hash and set to instance variable so user data may be rendered in view file (/users/home)
   end
 end
